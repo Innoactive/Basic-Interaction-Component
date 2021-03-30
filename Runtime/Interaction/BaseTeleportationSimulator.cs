0047913b67fb9c66e40c4631b4f264078a66925c
Gustavo Quiroz <gusinuhe@gmail.com>
2021-03-31 00:57:10 +0200

    feat: new abstract teleportation simulator

---
+++ Runtime/Interaction/BaseTeleportationSimulator.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using Innoactive.Creator.Core.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Innoactive.Creator.BasicInteraction
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class BaseTeleportationSimulator
+    {
+        private static BaseTeleportationSimulator instance;
+        
+        /// <summary>
+        /// Current instance of the interaction simulator.
+        /// </summary>
+        public static BaseTeleportationSimulator Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Type type = ReflectionUtils
+                        .GetConcreteImplementationsOf(typeof(BaseTeleportationSimulator))
+                        .FirstOrDefault(t => t != typeof(TeleportationSimulatorDummy));
+
+                    if (type == null)
+                    {
+                        type = typeof(TeleportationSimulatorDummy);
+                    }
+
+                    instance = (BaseTeleportationSimulator)ReflectionUtils.CreateInstanceOfType(type);
+                    SceneManager.sceneUnloaded += OnSceneLoad;
+                }
+
+                return instance;
+            }
+        }
+
+        private static void OnSceneLoad(Scene scene)
+        {
+            instance = null;
+            SceneManager.sceneUnloaded -= OnSceneLoad;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public abstract Type GetTeleportationBaseType();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rig"></param>
+        /// <param name="teleportationObject"></param>
+        /// <param name="targetPosition"></param>
+        /// <param name="targetRotation"></param>
+        public abstract void Teleport(GameObject rig, GameObject teleportationObject, Vector3 targetPosition, Quaternion targetRotation);
+
+    }
+}
---
+++ Runtime/Interaction/TeleportationSimulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Innoactive.Creator.BasicInteraction
+{
+    /// <summary>
+    /// Interaction simulator dummy, does nothing beside of warning to there is no concrete implementation of a simulator.
+    /// </summary>
+    public class TeleportationSimulatorDummy : BaseTeleportationSimulator
+    {
+        private const string ErrorMessage = "You are using the teleportation simulator without providing a concrete implementation for your VR interaction framework.";
+        
+        /// <inheritdoc />
+        public override Type GetTeleportationBaseType()
+        {
+            Debug.LogWarning(ErrorMessage);
+            return null;
+        }
+
+        /// <inheritdoc />
+        public override void Teleport(GameObject rig, GameObject teleportationObject, Vector3 targetPosition, Quaternion targetRotation)
+        {
+            Debug.LogWarning(ErrorMessage);
+        }
+    }
+}
