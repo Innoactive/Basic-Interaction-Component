c0c266fbf8016458cc9db6974c4f98ace5b3bef4
Thomas W <tomwim@users.noreply.github.com>
2021-04-06 11:53:38 +0200

    docs: fix

--- Runtime/Interaction/TeleportationSimulator.cs
+++ Runtime/Interaction/TeleportationSimulator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Innoactive.Creator.BasicInteraction
 {
     /// <summary>
-    /// Interaction simulator dummy, does nothing beside of warning to there is no concrete implementation of a simulator.
+    /// Interaction simulator dummy. Does nothing besides warning that there is no concrete implementation of a <see cref="BaseTeleportationSimulator"/>.
     /// </summary>
     public class TeleportationSimulatorDummy : BaseTeleportationSimulator
     {
